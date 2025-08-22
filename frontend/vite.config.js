54e6cc462f6011e7ec3b8be14bd19e35734d27fc
Karanpatel10 <kbp4451@gmail.com>
2025-08-21 18:09:08 -0700

    Fix base path for Vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/FoodDelivery/',
+  base:'/',
   plugins: [react(),tsconfigPaths()],
 })
